c1f70ada4b72795eaaa1dff3326f198ec3969a09
minanadynarouz <mina.n.narouz@gmail.com>
2023-10-31 23:19:53 -0700

    amend 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 i = 0
+x = 1
 for num in range(122, 96, -1):
-    print("{0}".format(chr(num - i)), end="")
-    if i == 0:
+    print("{}".format(chr(num - i)), end="")
+    x += 1;
+    if (x % 2) == 0:
         i = 32
     else:
         i = 0
