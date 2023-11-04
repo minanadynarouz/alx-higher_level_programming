0d3271028f14764fcd64b97a84b7755c2b2b9495
minanadynarouz <mina.n.narouz@gmail.com>
2023-11-04 01:16:46 -0700

    submit 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1, sys
+    argsLen = len(sys.argv)
+
+    if argsLen != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if op == "+":
+        print(f"{a} {op} {b} = {a + b}")
+    elif op == "-":
+        print(f"{a} {op} {b} = {a - b}")
+    elif op == "*":
+        print(f"{a} {op} {b} = {a * b}")
+    elif op == "/":
+        print(f"{a} {op} {b} = {a / b}")
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
