3a8a0a2f31074c195128697a09150c454c75f58b
minanadynarouz <mina.n.narouz@gmail.com>
2023-11-07 21:07:10 -0800

    amending performance

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,14 +9,21 @@
 
 int is_palindrome(listint_t **head)
 {
-	int *ll_arr;
+	int *ll_arr, count = 0;
 	size_t i = 0, start, end;
 	listint_t *current = *head;
 
-	ll_arr = malloc(sizeof(listint_t));
-	if (ll_arr == NULL || *head == NULL)
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+
+	ll_arr = malloc(sizeof(listint_t) * count);
+	if (ll_arr == NULL || *head == NULL || (*head)->next == NULL)
 		return (1);
 
+	current = *head;
 	while (current != NULL)
 	{
 		ll_arr[i] = current->n;
@@ -24,20 +31,12 @@ int is_palindrome(listint_t **head)
 		i++;
 	}
 
-	if (i % 2 == 0)
+	for (start = 0, end = i - 1; start < end; start++, end--)
 	{
-		for (start = 0, end = i - 1; start < i / 2 && end >= i / 2; start++, end--)
+		if (ll_arr[start] != ll_arr[end])
 		{
-			if (ll_arr[start] != ll_arr[end])
-				return (0);
-		}
-	}
-	else
-	{
-		for (start = 0, end = i - 1; start < i / 2 && end > i / 2; start++, end--)
-		{
-			if (ll_arr[start] != ll_arr[end])
-				return (0);
+			free(ll_arr);
+			return (0);
 		}
 	}
 	free(ll_arr);
