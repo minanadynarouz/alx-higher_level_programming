5e83f4b047ad9f8237b73bfc81d743a180dff9a3
minanadynarouz <mina.n.narouz@gmail.com>
2023-11-20 15:05:32 -0800

    amend

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    boolean = True
     try:
-        print("{}".format(int(value)))
+        print("{:d}".format(value))
+        return True
     except ValueError:
-        boolean = False
-    return boolean
+        return False
