e0e9b3d3fb5ff73b4140090b6fc04c6342638abb
minanadynarouz <mina.n.narouz@gmail.com>
2023-11-23 15:10:46 -0800

    amend

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """class square that define a square dimentions"""
     def __init__(self, size=0):
-        if !isdigit(size):
+        if isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise TypeError("size must be >= 0")
