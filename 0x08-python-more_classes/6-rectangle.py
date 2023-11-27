436ba117dd560c5110fdfc220616874320c678b1
minanadynarouz <mina.n.narouz@gmail.com>
2023-11-27 15:36:54 -0800

    amend

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """Rectangle class with setters and getters"""
+
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
@@ -14,7 +15,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -74,4 +75,4 @@ class Rectangle:
     def __del__(self):
         """Define destructor print message"""
         print("Bye rectangle...")
-        number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
