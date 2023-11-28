c5f27f066353f4dacd1d084adda49408ca2d59d1
minanadynarouz <mina.n.narouz@gmail.com>
2023-11-28 12:53:44 -0800

    amendments

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,26 +1,35 @@
 def matrix_divided(matrix, div):
-  result_matrix = []
-  for row in matrix:
-    if not isinstance(row, list):
-      raise TypeError(
-          "matrix must be a matrix (list of lists) of integers/floats")
+    """Divide all elements of a matrix.
+    Args:
+        matrix (list): A list of lists of ints or floats.
+        div (int/float): The divisor.
+    Raises:
+        TypeError: If the matrix contains non-numbers.
+        TypeError: If the matrix contains rows of different sizes.
+        TypeError: If div is not an int or float.
+        ZeroDivisionError: If div is 0.
+    Returns:
+        A new matrix representing the result of the division.
+    """
+    result_matrix = []
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a matrix (list of lists) "
+                    "of integers/floats")
 
-  row_size = len(matrix[0])
-  for row in matrix:
-    if len(row) != row_size:
-      raise TypeError("Each row of the matrix must have the same size")
-    else:
-      for item in row:
-        if not isinstance(item, (int, float)):
-          raise TypeError(
-              "matrix must be a matrix (list of lists) of integers/floats")
+    row_size = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_size:
+            raise TypeError("Each row of the matrix must have the same size")
+        else:
+            for item in row:
+                if not isinstance(item, (int, float)):
+                    raise TypeError("matrix must be a matrix (list of lists) "
+                            "of integers/floats")
 
-  if not isinstance(div, (int, float)):
-    raise TypeError("div must be a number")
-  elif div == 0:
-    raise ZeroDivisionError("division by zero")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    elif div == 0:
+        raise ZeroDivisionError("division by zero")
 
-  for row in matrix:
-      result_matrix.append([float("{:.2f}".format(item / div)) for item in row])
-
-  print(result_matrix)
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    """Function to return formatted name
+    Args:
+        first_name (str) -> The first name
+        last_name (str)  -> The last name
+    Returns:
+        'My name is <first_name> <last_name>'
+    """
+    try:
+        if not isinstance(first_name, str):
+            raise TypeError("first_name must be a string")
+        if not isinstance(last_name, str):
+            raise TypeError("last_name must be a string")
+        else:
+            return ("My name is {} {}".format(first_name, last_name))
+    except Exception as e:
+        return (e)
