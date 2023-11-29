ed7ce865698328588d4b573c0bdee76cc15407de
minanadynarouz <mina.n.narouz@gmail.com>
2023-11-29 12:54:02 -0800

    amend

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
+"""Function that says my name"""
+
+
 def say_my_name(first_name, last_name=""):
-    """Function to return formatted name
-    Args:
-        first_name (str) -> The first name
-        last_name (str)  -> The last name
-    Returns:
-        'My name is <first_name> <last_name>'
-    """
-    try:
-        if not isinstance(first_name, str):
-            raise TypeError("first_name must be a string")
-        if not isinstance(last_name, str):
-            raise TypeError("last_name must be a string")
-        else:
-            return ("My name is {} {}".format(first_name, last_name))
-    except Exception as e:
-        return (e)
+    """string arguments validation"""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
