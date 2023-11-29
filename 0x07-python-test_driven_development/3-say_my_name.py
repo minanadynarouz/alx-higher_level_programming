cc5010fbd9388227f9c15439b4773dd08506ae89
minanadynarouz <mina.n.narouz@gmail.com>
2023-11-29 12:55:17 -0800

    l

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,8 +4,11 @@
 
 def say_my_name(first_name, last_name=""):
     """string arguments validation"""
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    elif not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    try:
+        if not isinstance(first_name, str):
+            raise TypeError("first_name must be a string")
+        elif not isinstance(last_name, str):
+            raise TypeError("last_name must be a string")
+        print("My name is {} {}".format(first_name, last_name))
+    except Exception as e:
+        print(e)
