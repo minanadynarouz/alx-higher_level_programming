16e2caba217c66abe20b92e50427844ed0fb3741
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-01 00:52:03 -0800

    amend

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,44 +1,49 @@
 #!/usr/bin/python3
-"""Get the product of two matrices together"""
+"""Module matrix_mul
+Multiplies two matrices and returns the result.
+"""
 
 
 def matrix_mul(m_a, m_b):
-    """Each Matrix m_a and m_b is a list of lists.
-       Number of columns in m_a has to be equal number of Rows in m_b
-    Args:
-        m_a (list): Matrix A
-        m_b (list): Matrix B
-    Returns:
-        list: Product of Matrix A and Matrix B
-    """
+    """Return the matrix resulting of
+    the multiplication of m_a and m_b."""
 
-    if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list of lists")
-    elif not isinstance(m_b, list):
-        raise TypeError("m_b must be a list of lists")
-
-    if not all(isinstance(row, list) for row in m_a):
+    if type(m_a) is not list:
         raise TypeError("m_a must be a list")
-    elif not all(isinstance(row, list) for row in m_b):
+    if type(m_b) is not list:
         raise TypeError("m_b must be a list")
 
-    if not all(isinstance(num, int) for row in m_a for num in row):
-        raise TypeError("m_a should contain only integers or floats")
-    elif not all(isinstance(num, int) for row in m_b for num in row):
-        raise TypeError("m_b should contain only integers or floats")
+    for x in m_a:
+        if type(x) is not list:
+            raise TypeError("m_a must be a list of lists")
+    for x in m_b:
+        if type(x) is not list:
+            raise TypeError("m_b must be a list of lists")
 
-    if len(m_a[0]) == 0 or len(m_a) == 0:
+    if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
-    elif len(m_b[0]) == 0 or len(m_b) == 0:
+    if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
 
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b must have the same number of columns")
+    for row in m_a:
+        for x in row:
+            if type(x) is not int and type(x) is not float:
+                raise TypeError("m_a should contain only integers or floats")
+    for row in m_b:
+        for x in row:
+            if type(x) is not int and type(x) is not float:
+                raise TypeError("m_b should contain only integers or floats")
 
-    if any(len(row) != len(m_a[0]) for row in m_a):
-        raise ValueError("each row of m_a must be of the same size")
-    if any(len(row) != len(m_b[0]) for row in m_b):
-        raise ValueError("each row of m_b must be of the same size")
+    row_len = []
+    for row in m_a:
+        row_len.append(len(row))
+    if not all(elem == row_len[0] for elem in row_len):
+            raise TypeError("each row of m_a must should be of the same size")
+    row_len = []
+    for row in m_b:
+        row_len.append(len(row))
+    if not all(elem == row_len[0] for elem in row_len):
+            raise TypeError("each row of m_b must should be of the same size")
 
     a_col = 0
     for col in m_a[0]:
