d29603a29738e0d0964b310852c093bfe41c8ab6
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-04 06:41:59 -0800

    amend

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """MyInt is a rebel. MyInt has == and != operators inverted"""
 
+
 class MyInt(int):
     """Class inherit from int, but invert == and !="""
-    def __eq__(self, val):
-    """Equality becomes inequality"""
-        return super().__ne__(val)
 
-    def __ne__(self, val):
-    """Inequality becomes equality"""
-        return super().__eq__(val)
+
+    def __eq__(self, other):
+        """Equality becomes inequality"""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Inequality becomes equality"""
+        return super().__eq__(other)
