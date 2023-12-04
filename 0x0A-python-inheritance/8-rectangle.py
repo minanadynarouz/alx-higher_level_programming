c0e4a8f60774f90417cb5bfac8e90525e138ee98
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-04 05:54:08 -0800

    submit 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Child Class Rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """Represents Rectangle and inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Constructor"""
+
+        if self.integer_validator("Width", width):
+            self.__width = width
+
+        if self.integer_validator("Height", height):
+            self.__height = height
