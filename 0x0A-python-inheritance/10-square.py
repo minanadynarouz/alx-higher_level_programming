6e9cec5a5b99b79e1681c265cab833fadcc6d92c
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-04 06:13:13 -0800

    submit 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Child Class Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents Square and inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Constructor"""
+        super().__init__(size, size)
+        
+        self.integer_validator("size", size)
+        self.__size = size
