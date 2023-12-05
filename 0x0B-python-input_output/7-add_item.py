4065d0e9f4757c4c9784d3db56580179f41ebf7f
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-05 09:37:43 -0800

    amend

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,6 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-
 def main():
     try:
         oldContent = load_from_json_file("add_item.json")
@@ -20,4 +19,6 @@ def main():
 
     save_to_json_file(newContent, "add_item.json")
 
-main()
+
+if __name__ == '__main__':
+    main()
