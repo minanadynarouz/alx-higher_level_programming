16e643db7a602958078555dcb8a718811dd1ac7b
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-05 09:32:53 -0800

    amend

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 """Add all arguments to a list and save them to a .json"""
+from sys import argv
+import json
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-from sys import argv
 
 
 def main():
