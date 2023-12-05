7fd912a7284c9661a9ba71a18db6b9d82260f71c
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-05 07:29:06 -0800

    amend

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,10 +4,12 @@
 
 
 def write_file(filename="", text=""):
-    """ Append to the end of the file
-    :param filename:
-    :param text:
-    :return: Count of written letters
+    """Apend <text> to end of <filename>
+    Args:
+        filename(str) -> Name of the file to append to
+        text(str)     -> Content to write
+    Returns:
+        Number of characters written
     """
     if filename:
         with open(filename, "a", encoding="utf-8") as f:
