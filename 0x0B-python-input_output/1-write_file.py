1f8b80212853beb89cc0c5dfe78543d690c89779
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-05 07:20:02 -0800

    amend

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,13 @@
 
 
 def write_file(filename="", text=""):
+    """Write <text> to <filename> overriding
+    Args:
+        filename(str) -> Name of the file to write to
+        text(str)     -> Content to write
+    Returns:
+        Number of characters written
+    """
     if filename:
         with open(filename, "w", encoding="utf-8") as f:
             oldData = f.tell()
