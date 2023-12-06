81775f0fa0df304c251fcf239e1a5285f8158d67
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-05 18:44:07 -0800

    amend

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-
-"""returns the dictionary description with simple data structure"""
-import json
-
+"""Return the dictionary description of an object"""
 
 
 def class_to_json(obj):
-    """Use __dict__ to get dict representation of object
-    Args:
-        obj -> is class
-    Return: 
-        Dict object representation
-    """
-    
+    """Dict description"""
     return obj.__dict__
