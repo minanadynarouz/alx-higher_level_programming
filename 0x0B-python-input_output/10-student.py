18647ee9e4166a35d0092f509a01ca6ce700f031
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-05 18:59:54 -0800

    amend

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Represent student"""
+    def __init__(self, first_name, last_name, age):
+        """Constructor - Initialize Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dict representation of a instance"""
+        if(type(attrs) == list and all(type(ele) == str for ele in attrs)):
+            return {key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)}
+        return self.__dict__
