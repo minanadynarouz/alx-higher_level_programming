41b6bdf7adfcbd92a68e85f11a5436334fe0614c
minanadynarouz <mina.n.narouz@gmail.com>
2024-01-17 20:41:03 -0800

    a

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- Select max temp in table
-SELECT state, MAX(state) as max_val
-FROM temperatures
-GROUP BY state
-ORDER BY max_val DESC;
+SELECT `state`, max(value) as `max_temp`
+FROM `temperatures`
+GROUP BY `state`
+ORDER BY `state`;
