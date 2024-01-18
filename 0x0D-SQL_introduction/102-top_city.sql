ebee0391116c5f838fa8d7fc98973a6822667a7e
minanadynarouz <mina.n.narouz@gmail.com>
2024-01-17 20:25:23 -0800

    amend

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- script that displays the top 3 of cities temperature during July and August
+SELECT `city`, AVG(`value`) as tmp_avg
+FROM `temperatures`
+WHERE `month` = 7 or `month` = 8
+GROUP BY `city`
+ORDER BY tmp_avg DESC
+LIMIT 3;
