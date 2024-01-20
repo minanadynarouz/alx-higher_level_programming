2d2416078920093845397ec942507e9fa5a118bd
minanadynarouz <mina.n.narouz@gmail.com>
2024-01-20 03:03:04 -0800

    aend

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,8 +2,8 @@
 SELECT gen.name
 FROM tv_genres AS gen
 JOIN tv_show_genres AS sh_gen
-	ON gen.id = sh_gen.id
+	ON gen.id = sh_gen.genre_id
 JOIN tv_shows AS sh
-	ON sh.id = sh_gen.id
+	ON sh.id = sh_gen.show_id
 WHERE sh.title = 'Dexter'
 ORDER BY gen.name;
