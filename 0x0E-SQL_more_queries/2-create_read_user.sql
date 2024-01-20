09702c36faf2e9db41a39941eb6159c4f86af7ac
minanadynarouz <mina.n.narouz@gmail.com>
2024-01-19 20:57:29 -0800

    amend

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- creates the database hbtn_0d_2 and the user user_0d_2
+CREATE USER user_0d_2@localhost
+IDENTIFIED BY 'user_0d_2_pwd';
+
+CREATE DATABASE hbtn_0d_2;
+
+GRANT select
+ON hbtn_0d_2.*
+TO user_0d_2@localhost;
