4a4426e585c1a609226c6a9f1e690f72af5fdf56
minanadynarouz <mina.n.narouz@gmail.com>
2024-01-19 21:02:44 -0800

    amend

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,4 @@
--- creates the MySQL server user user_0d_2
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
--- creates the database hbtn_0d_2
-CREATE DATABASE hbtn_0d_2;
-
--- grants select privileges on the hbtn_0d_2 database to user_0d_2
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-
+-- creates the MySQL server user user_0d_2 & database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY user_0d_2_pwd;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
