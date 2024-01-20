cd11c5850ffddf4324893c909fb8c59c92fc4119
minanadynarouz <mina.n.narouz@gmail.com>
2024-01-20 03:40:55 -0800

    amend

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,9 +1,9 @@
--- ist all genres not linked to the show Dexter.
+-- List all genres not linked to the show Dexter.
 SELECT gen.name
 FROM tv_genres AS gen
 JOIN tv_show_genres AS sh_gen
 	ON gen.id = sh_gen.genre_id
 JOIN tv_shows AS sh
-	ON sh.id NOT = sh_gen.genre_id
+	ON sh.id <> sh_gen.genre_id
 WHERE sh.title = 'Dexter'
 ORDER BY gen.name;
