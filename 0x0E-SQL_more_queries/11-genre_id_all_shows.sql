162867e83ec6c9a28b688f23d1c0f093879551ed
minanadynarouz <mina.n.narouz@gmail.com>
2024-01-19 23:27:17 -0800

    amend

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_show.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
