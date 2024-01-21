9e9871fdf2284c2f2818e7595539e49188c53f29
minanadynarouz <mina.n.narouz@gmail.com>
2024-01-20 20:39:12 -0800

    amend

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,12 +1,11 @@
--- lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
-SELECT @comedy_genre_id := id
-FROM tv_genres
-WHERE name = 'Comedy';
+-- Declare and set the variable in the same session
+SET @comedy_genre_id := (SELECT id FROM tv_genres WHERE name = 'Comedy');
 
--- List all show ids without the genre Comedy
+-- List all show titles without the genre Comedy
 SELECT DISTINCT tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_show_genres
-    ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id != @comedy_genre_id OR tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title;
+
