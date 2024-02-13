6ae91c1d48d6875683312b2a1eec40be160a2330
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-12 18:27:32 -0800

    amend

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,7 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = () => { myObject.value++; };
+myObject.incr = () => { 
+  myObject.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
