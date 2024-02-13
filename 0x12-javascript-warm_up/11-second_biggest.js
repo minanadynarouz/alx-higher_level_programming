1f1898ffbfe11b1cc7288a0bd1d16a2866548794
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-12 18:08:35 -0800

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const len = process.argv.length;
+let arr = process.argv.slice(2).map(Number);
+
+if (len <= 2 || len === 3) {
+  console.log(0);
+} else {
+  arr.sort((a, b) => b - a);
+  console.log(arr[1]);
+}
