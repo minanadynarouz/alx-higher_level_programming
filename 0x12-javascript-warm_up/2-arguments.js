3897b8cc31c39e6a5b2165040067b0169a54c583
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-12 16:30:26 -0800

    Task 2 & 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
-
-let length = process.argv.length;
-let args = process.argv;
+const length = process.argv.length;
 
 if (length < 3) {
-	console.log("No argument");
+  console.log('No argument');
 } else if (length === 3) {
-	console.log("Argument found");
+  console.log('Argument found');
 } else {
-	console.log("Arguments found");
+  console.log('Arguments found');
 }
