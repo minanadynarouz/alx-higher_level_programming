5e535f930a9150acfd3854c8ef85edc9424e5e8f
Mina Nady Narouz <131834381+minanadynarouz@users.noreply.github.com>
2024-02-14 12:37:28 +0200

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let i = 0; i < h; i++) {
+      for (let j = 0; j < w; j++) {
+        console.log('X');
+      }
+      console.log();
+    }
+  }
+};
