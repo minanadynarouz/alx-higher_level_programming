a88aab36127dd7a7dc269ea2c74117e3322d5537
Mina Nady Narouz <131834381+minanadynarouz@users.noreply.github.com>
2024-02-14 13:49:51 +0200

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   let newList = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    newList.push(list[i]);
+    newList.push (list[i]);
   }
   return newList;
 };
