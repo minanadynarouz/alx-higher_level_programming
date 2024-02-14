74e174a091b13058ed78fbe898e8ca931cc86646
Mina Nady Narouz <131834381+minanadynarouz@users.noreply.github.com>
2024-02-14 13:47:24 +0200

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-exports.esrever = function (list) {
-  return list.reverse();
+function esrever (list) {
+  let newList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return newList;
 };
