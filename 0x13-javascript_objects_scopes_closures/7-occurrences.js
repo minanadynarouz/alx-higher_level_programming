f2495ded0dcdc9c85619e857e94c7b56ef78d760
Mina Nady Narouz <131834381+minanadynarouz@users.noreply.github.com>
2024-02-14 13:06:09 +0200

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let counter = 0;
 
-  for (let i = 0; i < list.length - 1; i++) {
+  for (let i = 0; i < list.length; i++) {
     if (searchElement === list[i]) {
       counter++;
     }
