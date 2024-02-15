057718f741fda2f04ddcc9899c2ba69d632be5b2
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-14 22:06:42 -0800

    Adding file

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,5 +2,5 @@
 
 const importedList = require('./100-data.js').list;
 let newArr = importedList.map((num, idx) => num * idx);
-console.log(importeList);
+console.log(importedList);
 console.log(newArr);
