9fe1cb6ce92a5ac3b6c5230991d7478f3c2190ea
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-14 22:09:00 -0800

    Adding file

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const importedList = require('./100-data.js').list;
-const newArr = importedList.map((num, idx) => num * idx);
+let newArr = importedList.map((num, idx) => num * idx);
 console.log(importedList);
 console.log(newArr);
