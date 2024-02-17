4aee90bc9f9870e4c24b22a122a62d7bde9714d3
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-17 10:38:02 -0800

    amenddment

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 lists all states from the database hbtn_0e_0_usa
 Usage: ./0-select_states.py <username> \
@@ -9,9 +10,16 @@ Usage: ./0-select_states.py <username> \
 import sys
 import MySQLdb as mydb
 
+
 def connect_execute():
+
+    """Connect to the database and execute query"""
     try:
-        db = mydb.connect(user=sys.argv[1], passwd=sys.argv[2], database=sys.argv[2])
+        db = mydb.connect(
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                database=sys.argv[2]
+                )
         cur = db.cursor()
         cur.execute('SELECT * FROM states ORDERBY `id` ASC;')
         states = cur.fetchall()
@@ -20,6 +28,7 @@ def connect_execute():
             print(state)
     except Exception as e:
         return (e)
+
 
 if __name__ = "__main__":
     connect_execute()
