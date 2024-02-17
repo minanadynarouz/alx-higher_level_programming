82011a0b23b72c962d42d8005d7ed2e87fa9d730
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-17 03:03:43 -0800

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,25 @@
-asdasdasdasdaaTETST
+#!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+Usage: ./0-select_states.py <username> \
+                            <password> \
+                             <database-name>
+"""
+
+import sys
+import MySQLdb as mydb
+
+def connect_execute():
+    try:
+        db = mydb.connect(user=sys.argv[1], passwd=sys.argv[2], database=sys.argv[2])
+        cur = db.cursor()
+        cur.execute('SELECT * FROM states ORDERBY `id` ASC;')
+        states = cur.fetchall()
+
+        for state in states:
+            print(state)
+    except Exception as e:
+        return (e)
+
+if __name__ = "__main__":
+    connect_execute()
