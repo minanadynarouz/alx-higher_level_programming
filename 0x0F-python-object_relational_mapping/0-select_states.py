cb9b13f6965faabdfd2aa764a4e7c8d6e6d4b97f
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-17 11:37:21 -0800

    amend

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,13 @@
 #!/usr/bin/python3
-
-"""
-Lists all states from the states table of database hbtn_0e_0_usa.
-Usage: ./0-select_states.py <username> \
-                            <password> \
-                             <database-name>
-"""
-import sys
+"""lists all states from the database"""
 import MySQLdb
-
-
-def connect_query():
-
-    """Connect to the database and execute query"""
-    try:
-        db = MySQLdb.connect(host="localhost",
-                port=3306,
-                user=sys.argv[1],
-                passwd=sys.argv[2],
-                db=sys.argv[3]
-                )
-        cur = db.cursor()
-        rows = cur.execute('SELECT * FROM states ORDER BY states.id')
-
-        for i in range(rows):
-            print(cur.fetchone())
-    except Exception as e:
-        return (e)
+from sys import argv
 
 
 if __name__ == "__main__":
-    connect_query()
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
+    for i in range(num_rows):
+        print(cur.fetchone())
