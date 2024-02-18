5a8f504e137d90bb8a11077cabe9c04a12a7299d
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-18 08:30:45 -0800

    a

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -23,4 +23,4 @@ class State(Base):
     name = Column(
             String(128),
             nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship("City", backref="state", cascade="all")
