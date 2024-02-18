9b342d03d8feb1ab23d8b20f74bbaa8e97abf19e
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-18 07:06:25 -0800

    a

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, database), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    updateState = session.query(State).filter(State.id == 2)
+    updateState.name = "New Mexico"
+    session.commit()
+
+    session.close()
