301d3b099a63ac45d983bfc98c82b279297ae980
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-18 01:28:21 -0800

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""MySQLdb script"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    matchName = sys.argv[4]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+    )
+    cur = conn.cursor()
+
+    query = "SELECT * FROM states WHERE name = %s"
+    cur.execute(query, (matchName,))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
