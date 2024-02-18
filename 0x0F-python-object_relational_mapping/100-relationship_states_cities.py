73f3c96c53f1363f3b080b6f0180b01115631ece
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-18 08:28:08 -0800

    amend

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -11,12 +11,18 @@ from relationship_state import State
 from relationship_city import Base, City
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           .format(username, passwd, db),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    session.add(City(name="San Francisco", state=State(name="California")))
+    newState = City(name="San Francisco", state=State(name="California"))
+    session.add(newState)
     session.commit()
+
+    session.close()
