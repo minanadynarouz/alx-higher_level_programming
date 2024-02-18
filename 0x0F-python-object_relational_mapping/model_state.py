dbcf500c485a1392305e1f56ea6c5febd59672ad
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-18 04:34:34 -0800

    amend

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,16 +8,18 @@ import MySQLdb
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
 
+
 class State(Base):
-   """Table Declaration""" 
-   __tablename__ = 'states'
-   id = Column(Integer,
-           unique=True, 
-           autoincrement=True,
-           primary_key=True,
-           nullable=False
-           )
-   name = Column(String(128), nullable=False)
+    """Table Declaration"""
+    __tablename__ = 'states'
+    id = Column(
+            Integer,
+            unique=True,
+            autoincrement=True,
+            primary_key=True,
+            nullable=False)
+    name = Column(
+            String(128),
+            nullable=False)
