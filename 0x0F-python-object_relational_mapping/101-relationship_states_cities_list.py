ab47cad473c403021f01e5a772ae3df0dbcfca38
Mina Nady Narouz <131834381+minanadynarouz@users.noreply.github.com>
2024-02-19 01:04:56 +0200

    Update 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             username, password, database), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
