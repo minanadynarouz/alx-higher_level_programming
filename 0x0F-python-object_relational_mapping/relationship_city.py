a57b81c0e9bbae21b747a0ea58b1691237bab75c
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-18 08:14:04 -0800

    a

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -12,17 +12,6 @@ from model_state import Base
 class City(Base):
     """Table Declaration"""
     __tablename__ = 'cities'
-    id = Column(
-        Integer,
-        primary_key=True,
-        unique=True,
-        autoincrement=True,
-        nullable=False)
-    name = Column(
-        String(128),
-        nullable=False)
-    state_id = Column(
-        Integer,
-        ForeignKey('states.id'),
-        nullable=False)
-    state = relationship('State')
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
