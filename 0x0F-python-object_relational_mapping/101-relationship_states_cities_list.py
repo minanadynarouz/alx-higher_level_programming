40d7cd74772667934de0779f3f53cb63b596012c
Mina Nady Narouz <131834381+minanadynarouz@users.noreply.github.com>
2024-02-19 01:10:58 +0200

    Update 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-
-"""
-adds the State object “Louisiana” to the database hbtn_0e_6_usa
-"""
+"""Lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa"""
 
 import sys
-from model_state import Base, State
-from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from relationship_state import State, Base
+from relationship_city import City
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    database = sys.argv[3]
+    db_name = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            username, password, database), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
+                        pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id).all():
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+            print("\t{}: {}".format(city.id, city.name))
+  
+    session.close()
