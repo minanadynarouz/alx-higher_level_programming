b7eecc99f90471ae62f65432c9fbf79da8c7aba9
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-29 03:24:56 -0800

    amend 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays the body of the response
-curl -s -X POST -d "email=test@gmail.com&subject=%20I%20will%20always%20be%20here%20for%20PLD" $1
+curl -s -X POST --data "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
