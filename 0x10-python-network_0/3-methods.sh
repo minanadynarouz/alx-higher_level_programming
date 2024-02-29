78a32bec9d1a26375f96be52fb43819e181de731
minanadynarouz <mina.n.narouz@gmail.com>
2024-02-29 03:15:56 -0800

    amend 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays the body of the response
-curl -sI OPTIONS $1 | grep -i "allow" | cut -d ":" -f 2
+curl -sI OPTIONS $1 | grep -i "allow" | cut -d " " -f 2-
