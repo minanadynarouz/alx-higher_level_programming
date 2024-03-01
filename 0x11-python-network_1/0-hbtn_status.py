ff0369a7c2664bf1626599aeee9a00428a35d726
minanadynarouz <mina.n.narouz@gmail.com>
2024-03-01 02:58:09 -0800

    amend

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,16 @@
 """Script to fetch URL"""
 
 import urllib.request as Req
-
+import urllib.error as Err
 
 if __name__ == '__main__':
-    with Req.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        res = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(res)))
-        print("\t- content: {}".format(res))
-        print("\t- utff8 content: {}".format(res.decode('utf-8')))
+    URL = 'https://alx-intranet.hbtn.io/status'
+    try:
+        with Req.urlopen(URL) as response:
+            res = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(res)))
+            print("\t- content: {}".format(res))
+            print("\t- utff8 content: {}".format(res.decode('utf-8')))
+    except Err.URLError as e:
+        print("Cannot connect to {}".format(URL))
