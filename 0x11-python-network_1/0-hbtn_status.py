1813caa1c58296733ddce2de11be2b580de51ecd
minanadynarouz <mina.n.narouz@gmail.com>
2024-03-01 03:03:29 -0800

    amend

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,14 +3,15 @@
 
 from urllib import request, error
 
+
 if __name__ == "__main__":
-    URL = "https://alx-intranet.hbtn.io/status"
+    url = "https://alx-intranet.hbtn.io/status"
     try:
-        with Req.urlopen(URL) as response:
+        with request.urlopen(url) as response:
             res = response.read()
             print("Body response:")
             print("\t- type: {}".format(type(res)))
             print("\t- content: {}".format(res))
-            print("\t- utff8 content: {}".format(res.decode('utf-8')))
+            print("\t- utf8 content: {}".format(res.decode('utf-8')))
     except error.URLError:
-        print("Cannot connect to {}".format(URL))
+        print("Cannot connect to {}".format(url))
