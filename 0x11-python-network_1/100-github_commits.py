abb41d0af6fc25e2618b87f6eadbedf0c0fe9d34
minanadynarouz <mina.n.narouz@gmail.com>
2024-03-01 17:40:27 -0800

    task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script to fetch URL using requests package"""
+
+import requests
+from sys import argv
+
+
+def git_commits(repo_name: str, ownerName: str):
+    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
+    req = requests.get(url)
+    commits = req.json()
+
+    for x in range(10):
+        print("{}: {}".format(
+            commits[x].get("sha"),
+            commits[x].get("commit").get("author").get("name")))
+
+if __name__ == "__main__":
+    git_commits(argv[2], argv[1])
