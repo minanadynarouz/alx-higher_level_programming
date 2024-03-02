1a9f481e8c714789b7cb3546fc50f0ec66f00753
minanadynarouz <mina.n.narouz@gmail.com>
2024-03-02 03:32:56 -0800

    amend style

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,7 @@ from requests.auth import HTTPBasicAuth
 
 def usr_github_cred(user: str, passw: str):
     """
-    GitHub credentials (username and password) and 
+    GitHub credentials (username and password) and
     uses the GitHub API to display your id
     Args:
         user -> username
@@ -25,6 +25,7 @@ def usr_github_cred(user: str, passw: str):
     auth = HTTPBasicAuth(user, passw)
     resp = requests.get(api, headers=header, auth=auth)
     return (resp.json().get('id'))
+
 
 if __name__ == "__main__":
     print(usr_github_cred(argv[1], argv[2]))
