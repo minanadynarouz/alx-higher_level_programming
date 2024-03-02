26ffeb246655b2b310577dfdbff5e07abd9ebf14
minanadynarouz <mina.n.narouz@gmail.com>
2024-03-01 17:59:28 -0800

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script to fetch URL using requests package"""
+
+import requests
+from sys import argv
+
+
+def display_err(url):
+    req = requests.get(url) 
+    
+    if int(req.status_code) >= 400:
+        return ("Error code: {}".format(req.status_code))
+
+    return (req.text)
+
+
+if __name__ == "__main__":
+    print(display_err(argv[1]))
