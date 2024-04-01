eac17590a10e8dfad6ed819dfbfa8895a9cb8be5
minanadynarouz <mina.n.narouz@gmail.com>
2024-04-01 13:31:47 -0700

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('node:fs')
+const fileName = process.argv[2]
+
+fs.readFile(fileName, 'utf8', (err, data) => {
+	if (err) {
+		console.log(err);
+		return;
+	}
+	console.log(data)
+});
