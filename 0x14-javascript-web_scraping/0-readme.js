0b13de18ca1174bc13cba2dcbffae5aed7e9e21f
minanadynarouz <mina.n.narouz@gmail.com>
2024-04-01 13:37:25 -0700

    amend style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const fs = require('node:fs')
-const fileName = process.argv[2]
+const fs = require('node:fs');
+const fileName = process.argv[2];
 
 fs.readFile(fileName, 'utf8', (err, data) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log(data)
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
 });
