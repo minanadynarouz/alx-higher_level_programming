3da02b6e63912f02204c785c3bee1b1b20c27956
root <root@MinaNady>
2024-04-04 00:00:51 +0200

    amend character id

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,7 +1,7 @@
 $.ajax({
     url: "https://swapi-api.alx-tools.com/api/people/5/?format=json",
     success: (res) => {
-        console.log(res["name"]); // undefined
+        $("#character").text(res.name);
     }
 });
 
