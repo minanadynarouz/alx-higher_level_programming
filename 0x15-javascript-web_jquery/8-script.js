bac83dbcc78afee45cd349a80e8af726583938f3
root <root@MinaNady>
2024-04-04 00:09:11 +0200

    task 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+let i = 0;
+
+$.ajax({
+    url: "https://swapi-api.alx-tools.com/api/films/?format=json",
+    success: (res) => {
+        for (let film of res.results) {
+            $("#list_movies").append(`<li>${film.title}</li>`)
+        }
+    }
+});
